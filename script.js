61b5f4c62572aad3df3c169f931a0bd660f241ab
DaraElisa <daraezjunior@gmail.com>
2025-02-14 11:55:04 -0300

    mudanças

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ toggleBtn.onclick = function (){
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 50)
+    header.classList.toggle('rolagem', window.scrollY > 0)
 })
 
 
